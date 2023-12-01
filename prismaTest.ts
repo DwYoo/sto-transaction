808466e9828fb5d04b9663646c761b7697683c9f
uquant0507 <99530149+uquant0507@users.noreply.github.com>
2023-12-02 02:00:35 +0900

    error connecting to db via prisma

---
+++ prismaTest.ts
@@ -0,0 +1,21 @@
+// 예제 코드
+require('dotenv').config();
+
+import { PrismaClient } from './prisma/generated/client'
+const prisma = new PrismaClient();
+
+async function main() {
+  try {
+    const users = await prisma.wallet.findMany();
+    console.log(users);
+  } catch (error) {
+    throw error;
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+main()
+  .catch((error) => {
+    throw error;
+  });
